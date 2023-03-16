05574471d4bf06e2904baefa7d2eafd7dd0e61b2
EmmaKLofthouse <e.k.lofthouse@gmail.com>
2023-03-16 10:02:50 +0100

    saving spectra with noise

--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,5 @@
-#import matplotlib
-#matplotlib.use('agg')
+import matplotlib
+matplotlib.use('agg')
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -45,7 +45,7 @@ def read_spectra(training_list, test_list):
     Ns_MgII_data, zs_MgII_data  = [], []
 
     # Loop over hdf5 files
-    for hdf5file in specfiles[:5]:
+    for hdf5file in specfiles[:2]:
 
         print("Working with file: %s"%hdf5file)
         
@@ -93,11 +93,11 @@ def read_spectra(training_list, test_list):
             if not all(np.isfinite(out_flux)):
                 continue
                 
-            fluxdata.append(out_flux)
-            Ns_CIV_data.append(NCIV)
-            zs_CIV_data.append(zCIV)
-            Ns_MgII_data.append(NMgII)
-            zs_MgII_data.append(zMgII)
+            fluxdata.append(list(out_flux))
+            Ns_CIV_data.append(list(NCIV))
+            zs_CIV_data.append(list(zCIV))
+            Ns_MgII_data.append(list(NMgII))
+            zs_MgII_data.append(list(zMgII))
         
     return fluxdata, out_wave, vel, Ns_CIV_data, zs_CIV_data, Ns_MgII_data, zs_MgII_data 
 
@@ -255,9 +255,9 @@ def slice_input(data, wave, vel, slide_idx):
             MgII2803_present = [(wl > wave_slice[0]) & (wl < wave_slice[-1]) for wl in obs_MgII_2803_wave]  
 
             # Add slice to array of inputs
-            fluxslices.append(flux_slice)
-            velslices.append(vel_slice)
-            waveslices.append(wave_slice)
+            fluxslices.append(list(flux_slice))
+            velslices.append(list(vel_slice))
+            waveslices.append(list(wave_slice))
             
             # If there is no absorption present, flag it as 0
             allLines_present = CIV1548_present + CIV1550_present + MgII2796_present + MgII2803_present
@@ -298,8 +298,8 @@ def slice_input(data, wave, vel, slide_idx):
                 is_abs.append(3)
                 absInfo.append(['partial MgII',0,0, "spec_" + str(source)])
             
-    
-    chunks = dict(fluxslices = fluxslices, 
+
+    chunks = dict(fluxslices = fluxslices,
                   waveslices = waveslices, 
                   velslices = velslices, 
                   is_abs = is_abs, 
@@ -326,62 +326,9 @@ def split_samples(fluxdata, wave, vel, Ns_CIV_data, zs_CIV_data, Ns_MgII_data, z
 
 
     return trainSpec, testSpec
-
-def preprocess(chunks):
-    #balance samples so that there is roughly the same number of noise vs absorbers
-
-    fluxslicesAll = chunks['fluxslices']
-    velslicesAll  = chunks['velslices']
-    waveslicesAll = chunks['waveslices']
-    is_absAll     = chunks['is_abs']
-    absInfoAll    = chunks['absInfo']
-
-    #find number of e.g. CIV absorbers
-    nCIV = len(np.array(is_absAll)[np.array(is_absAll) == 1])
-    npartial = len(np.array(is_absAll)[np.array(is_absAll) == 3])
-
-    #find where the noise samples are
-    idxs_noise = np.where(np.array(is_absAll)==0)[0]
-
-    #check there are more noise chunks than CIV
-    if len(idxs_noise) > nCIV:
-
-        #randomly select nCIV indices from idxs_noise
-        idxs_to_delete = np.random.choice(idxs_noise, len(idxs_noise) - nCIV, replace=False)
-        #idxs_to_delete = np.random.choice(idxs_noise, len(idxs_noise) - npartial, replace=False)
-        
-        #delete indices from flux, vel, wave, is_abs and absInfo
-        fluxslices = list(np.delete(np.array(fluxslicesAll),idxs_to_delete,0))
-        velslices  = list(np.delete(np.array(velslicesAll),idxs_to_delete,0))
-        waveslices = list(np.delete(np.array(waveslicesAll),idxs_to_delete,0))
-        is_abs     = list(np.delete(np.array(is_absAll),idxs_to_delete,0))
-        absInfo    = list(np.delete(np.array(absInfoAll),idxs_to_delete,0))
-
-    else:
-        fluxslices = fluxslicesAll
-        velslices  = velslicesAll
-        waveslices = waveslicesAll
-        is_abs     = is_absAll
-        absInfo    = absInfoAll
-
-    return fluxslices, velslices, waveslices, is_abs, absInfo
- 
-def run_RF(train, train_isabs):
-
-    print("Build the forest...")
-    Forest=RandomForestClassifier(n_estimators=1000,criterion='gini',max_depth=None,
-                                  min_samples_split=10,min_samples_leaf=1,max_features=40,
-                                  max_leaf_nodes=None,bootstrap=True,oob_score=True,
-                                  n_jobs=40,random_state=120,verbose=0,class_weight='balanced')
-    print("Fit training sample...")
-    model=Forest.fit(train,train_isabs)
-
-    return model
+    
 
 ########################################################
-#scriptpath = os.path.dirname(os.path.abspath(__file__))
-#datapath = scriptpath + "/NMFPM_data/"
-#print(datapath)
 print("Reading spectra and adding noise...")
 
 training_list = '/data/tberg/WEAVE/Working/training_mocks.lst'
@@ -392,78 +339,37 @@ fluxdata, wave, vel, Ns_CIV_data, zs_CIV_data, Ns_MgII_data, zs_MgII_data = read
 print("Split train-test samples...")
 trainSpec, testSpec = split_samples(fluxdata, wave, vel, Ns_CIV_data, zs_CIV_data, Ns_MgII_data, zs_MgII_data)
 
+# Save spectra with noise
+import json
+with open("trainSpec.json", "w") as write_file:
+    json.dump(trainSpec, write_file, indent=4)
+with open("testSpec.json", "w") as write_file:
+    json.dump(testSpec, write_file, indent=4)
+
+"""
 print("Slicing spectra...")
 # Split spectra into chunks and assign flag for absorbers
 # Use a fine sliding for train sample but larger for test to avoid duplication
 trainChunks = slice_input(trainSpec, wave, vel, 5) # Give all the data and a 
                                                 # value to shift the window by
 testChunks = slice_input(testSpec, wave, vel, 50)
-testFineChunks = slice_input(testSpec, wave, vel, 5)
+testFineChunks = slice_input(testSpec, wave, vel, 5)    
 
-if __name__ == "__main__":
-    print("Preprocessing data...")
-    # e.g. balance samples
-    train, train_vel, train_wave, train_isabs, train_absInfo = preprocess(trainChunks)
-    test, test_vel, test_wave = testChunks['fluxslices'], testChunks['velslices'], testChunks['waveslices'] 
-    test_isabs, test_absInfo = testChunks['is_abs'], testChunks['absInfo']
-
-    testFine, testFine_vel, testFine_wave = testFineChunks['fluxslices'], testFineChunks['velslices'], testFineChunks['waveslices'] 
-    testFine_isabs, testFine_absInfo = testFineChunks['is_abs'], testFineChunks['absInfo']
-
-    print("Runnning Random Forest...")
-    model = run_RF(train, train_isabs)
-
-print("Making predictions...")
-# Classify whether test sample are absorber or not
-preds = model.predict(test)
-predsFine = model.predict(testFine)
-
-# If you want confidence, return probability of classes
-preds_probability = model.predict_proba(test)
-predsFine_probability = model.predict_proba(testFine)
-# Return the mean accuracy on the given test data and labels
-score = model.score(test,test_isabs)
-scoreFine = model.score(testFine, testFine_isabs)
-
-print("Creating recovery fraction plot for detection of metal types...")
-pl.plotRecoveryFraction_type(test_isabs,preds,test_absInfo)
-
-print("Creating identification plots...")
-pl.plotIdentifications(test_isabs,preds,test_absInfo)
-
-print("Plotting confusion matrix...")
-
-def plotCM(preds, test_isabs):
-    from sklearn.metrics import confusion_matrix
-    import seaborn as sn
-    from matplotlib.colors import SymLogNorm
-    preds_grouped = np.array(preds)
-    preds_grouped[preds_grouped >= 4] = 4
-    test_isabs_grouped = np.array(test_isabs)
-    test_isabs_grouped[test_isabs_grouped >= 4] = 4
-    cm = confusion_matrix(preds_grouped, test_isabs_grouped)
-    sn.heatmap(cm, 
-               annot=True, 
-               norm=SymLogNorm(linthresh=0.03, linscale=0.03, vmin=-1.0, 
-                               vmax=1e5, base=10),
-               cbar_kws={"ticks":[0,1,10,1e2,1e3,1e4]}, 
-               annot_kws={"size":8}, 
-               fmt='g')
-    plt.xlabel('True Class', fontsize=10)
-    plt.ylabel('Predicted Class', fontsize=10)
-    plt.savefig("cm_classifier.pdf")
-    plt.close()
-    
-    return
-plotCM(preds, test_isabs)
-
-print("Saving data...")
-utl.saveTrainData(train, train_isabs, train_absInfo, train_vel, train_wave, 
-              "train_data.pkl")
-utl.saveTestData(test, test_isabs, test_absInfo, test_vel, test_wave, preds, 
-             preds_probability, "test_data.pkl")
-utl.saveTestData(testFine, testFine_isabs, testFine_absInfo, testFine_vel, 
-             testFine_wave, predsFine, predsFine_probability, 
-             "testFine_data.pkl")
-
+import json
+with open("trainChunks.json", "w") as write_file:
+    json.dump(trainChunks, write_file, indent=4)
+with open("testFineChunks.json", "w") as write_file:
+    json.dump(testFineChunks, write_file, indent=4)
+with open("testChunks.json", "w") as write_file:
+    json.dump(testChunks, write_file, indent=4)
+"""
+"""
+# Save chunks to read into Random Forest Classifier in run_RF.py
+df_train = pd.DataFrame(data=trainChunks)
+df_train.to_pickle("trainChunks.pkl")
+df_test = pd.DataFrame(data=testChunks)
+df_test.to_pickle("testChunks.pkl")
+df_testFine = pd.DataFrame(data=testFineChunks)
+df_testFine.to_pickle("testFineChunks.pkl")
+"""
 
